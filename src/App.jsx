c3baacd8f6596bc48b218c907bb1afc35249c4c9
Luis Robles <luisgeremiasrobles@gmail.com>
2025-11-01 10:13:01 -0300

    Agregamos un componente footer y lo ubicamos al final de la pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Footer from "./components/Footer"
+
 function App() {
   return (
     <>
+      <header></header>
       <h1>Hola mundo!</h1>
+      <main></main>
+      <Footer />
     </>
   )
 }
